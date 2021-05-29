573438ee210bdf7c728a5ebead643248cd74d631
jantonio2 <jesusrochavedia@gmail.com>
2021-05-29 12:37:45 -0400

    Eventos click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+
+  // handleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { value } </h2>
+     {/* Cuando ponemos () nos saldra undefined al renderizar. Se esta pasando como referencia */}
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 }
