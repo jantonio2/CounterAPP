6d6462d881eae3e6ea7af63cf0fd6398ae5d92a1
jantonio2 <jesusrochavedia@gmail.com>
2021-05-29 10:44:40 -0400

    Fragments

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,6 +1,28 @@
+// import { Fragment } from "react";
+
 // FC Functional Components
 const PrimeraApp = () => {
-  return <h1>Hola Mundo</h1>;
+  // return (
+  //   <div>
+  //     <h1>Hola Mundo</h1>
+  //     <p>Mi primera aplicación</p>
+  //   </div>
+  // );
+    
+  // return (
+  //   <Fragment>
+  //     <h1>Hola Mundo</h1>
+  //     <p>Mi primera aplicación</p>
+  //   </Fragment>
+  // );
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Mi primera aplicación</p>
+    </>
+  );
+    
 }
 
 export default PrimeraApp;
