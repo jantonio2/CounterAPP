d3361e7e40788d183c8d2703509ee3306a5f01e0
jantonio2 <jesusrochavedia@gmail.com>
2021-05-29 12:49:24 -0400

    UseState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const CounterApp = ({value}) => {
 
+  const [counter, setCounter] = useState(0);
+
   // handleAdd
-  const handleAdd = (e) => {
-    console.log(e);
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    // setCounter( (c) => c + 1 );
   };
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { value } </h2>
+      <h2> { counter } </h2>
      {/* Cuando ponemos () nos saldra undefined al renderizar. Se esta pasando como referencia */}
       <button onClick={handleAdd}>+1</button>
     </>
