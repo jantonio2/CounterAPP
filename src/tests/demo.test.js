cadb960217441753d822b965ece1df2b0c9d5a20
jantonio2 <jesusrochavedia@gmail.com>
2021-06-16 18:41:02 -0400

    Aplicando expect-toBe

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,10 +1,13 @@
+describe('Pruebas en el archivo demo.test.js', () => {
+  test('deben de ser iguales los string', () => {
+    // 1. inicialización
+    const mensaje = 'Hola Mundo';
+  
+    // 2. Estímulo
+    const mensaje2 = `Hola Mundo`;
+  
+    // 3. Observar el comportamiento
+    expect(mensaje).toBe(mensaje2);// ===
+  })
+});
 
-test('debe de ser true', () => {
-
-  const isActive = true;
-
-  if (isActive){
-    throw new Error('No está activo');
-  }
-
-})
