34d3ff83b50328cb4f120fef7657d9e000831be1
jantonio2 <jesusrochavedia@gmail.com>
2021-09-18 22:16:18 -0400

    Usando enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,11 @@
 // Agregando por en toBeInTheDocument
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
+
+//npm install --save-dev enzyme poner esto a parte del install de git
+
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import {createSerializer} from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,15 +1,22 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import PrimeraApp from '../PrimeraApp';
+import { shallow } from 'enzyme';
 
 describe('Pruebas en <PrimeraApp />', () => {
-  test('debe mostrar el mensaje "Hola, Soy Antonio', () => {
+  // test('debe mostrar el mensaje "Hola, Soy Antonio', () => {
+  //   const saludo = 'Hola, Soy Antonio';
+  //   // const wrapper = render(<PrimeraApp saludo = {saludo} />);
+  //   const {getByText} = render(<PrimeraApp saludo = {saludo} />);
+  //   // wrapper.getByText();
+  //   expect(getByText(saludo)).toBeInTheDocument();
+  // })
+
+  test('debe de mostrar <PrimeraApp /> correctamente', () => {
     const saludo = 'Hola, Soy Antonio';
+    const wrapper = shallow( <PrimeraApp saludo = {saludo} /> );
 
-    // const wrapper = render(<PrimeraApp saludo = {saludo} />);
-    const {getByText} = render(<PrimeraApp saludo = {saludo} />);
-
-    // wrapper.getByText();
-    expect(getByText(saludo)).toBeInTheDocument();
+    expect(wrapper).toMatchSnapshot();
   })
+  
   
 })
