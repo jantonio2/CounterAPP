8a815dfadb5eb41067536324679f728bf2522e4a
jantonio2 <jesusrochavedia@gmail.com>
2021-09-19 00:25:50 -0400

    Probando botones +1 -1

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -2,9 +2,14 @@ import { shallow } from 'enzyme';
 import CounterApp from '../CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
-  test('debe de mostrar <CounterApp /> correctamente', () => {
-    const wrapper = shallow(<CounterApp />);
 
+  let wrapper = shallow(<CounterApp />);;
+
+  beforeEach(() => {
+    wrapper = shallow(<CounterApp />);
+  });
+
+  test('debe de mostrar <CounterApp /> correctamente', () => {
     expect(wrapper).toMatchSnapshot();
   });
   
@@ -18,6 +23,22 @@ describe('Pruebas en <CounterApp />', () => {
     const textoH2 = parseFloat(wrapper.find('h2').text().trim());
 
     expect(textoH2).toBe(100);
-  })
+  });
+
+  test('debe de incrementar con el botón de +1', () => {
+    wrapper.find('button').at(0).simulate('click');
+
+    const counterText = wrapper.find('h2').text().trim();
+
+    expect(counterText).toBe('11');
+  });
+
+  test('debe de decrementar con el botón de -1', () => {
+    wrapper.find('button').at(2).simulate('click');
+
+    const counterText = wrapper.find('h2').text().trim();
+
+    expect(counterText).toBe('9');
+  });
   
 })
